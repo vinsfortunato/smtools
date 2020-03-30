84f31d497b4797c7d29e479c329548aefd89fcdc
flood2d <vinsfortunato97@gmail.com>
2020-03-30 19:25:58 +0200

    Add copyright header Move into correct packages

--- src/main/java/com/vinsfortunato/smtools/task/ScanTask.java
+++ src/main/java/com/vinsfortunato/smtools/task/ScanTask.java
@@ -1,5 +1,22 @@
-package com.vinsfortunato.smtools;
+/*
+ * Copyright [2020] [Vincenzo Fortunato]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
+package com.vinsfortunato.smtools.task;
+
+import com.vinsfortunato.smtools.SimFormat;
 import javafx.concurrent.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +26,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.regex.Pattern;
 
 /**
  * Searches for sim file inside a given directory a return a list of them.
